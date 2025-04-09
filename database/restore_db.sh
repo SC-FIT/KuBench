8e5e1ebd653ba95de29e014e8b81e3b4b60f1f4a
Ondrej Olsak <iolsak@fit.vutbr.cz>
2025-04-09 10:30:22 +0200

    #1: Update restore_db.sh to use the correct dump file name for database restoration

--- database/restore_db.sh
+++ database/restore_db.sh
@@ -3,7 +3,7 @@ PSQL_USER="admin"
 POSTGRES_POD_NAME=$(kubectl get pods | grep postgres | awk '{print $1}' | head -n 1)
 PATH_TO_DUMP_FILE="database/experiment-dump.sql"
 
-kubectl cp $PATH_TO_DUMP_FILE $POSTGRES_POD_NAME:/tmp/no-data-dump.sql
+kubectl cp $PATH_TO_DUMP_FILE $POSTGRES_POD_NAME:/tmp/experiment-dump.sql
 kubectl exec -it $POSTGRES_POD_NAME -- bin/sh -c "
   psql -U $PSQL_USER -d $DATABASE_NAME -f /tmp/experiment-dump.sql
   "
